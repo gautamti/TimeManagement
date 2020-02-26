4fdd3ae21745279a4c0a393f24c31253f03922c5
Nate <myersnt@mail.uc.edu>
2020-02-26 18:10:25 -0500

    adds DTO's

---
+++ app/src/main/java/com/example/timemanagementapp/DTO/ActivityDTO.kt
@@ -0,0 +1,7 @@
+package com.example.timemanagementapp.DTO
+
+class ActivityDTO {
+    var activityName: String? = null
+    var activityDuration = 0
+
+}
---
+++ app/src/main/java/com/example/timemanagementapp/DTO/DailyActivityLogDTO.kt
@@ -0,0 +1,8 @@
+package com.example.timemanagementapp.DTO
+
+import java.util.*
+
+class DailyActivityLogDTO(
+    var date: Date,
+    var activityLog: List<ActivityDTO>
+)
